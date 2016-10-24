d011f8c788ff231053fbb6c97aaf3aee0cb84a2c
Apinan Woratrakun <me@apinu.com>
2016-10-24 23:14:58 +0700

    Create project

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created for someline-starter.
+ * User: Libern
+ */
+
+namespace Someline\Models;
+
+use Someline\Base\Models\BaseModel as Model;
+use Someline\Models\Foundation\User;
+
+class BaseModel extends Model
+{
+
+    /**
+     * @return User
+     */
+    public function getAuthUser()
+    {
+        return parent::getAuthUser();
+    }
+
+}
